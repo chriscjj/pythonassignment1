e5c6af10976566506f90ecb4c311618e0acb9cc9
chriscjj <chrisjohnjoy24@gmail.com>
2024-12-12 01:54:05 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,69 @@
+# Q1
+# print("Bob")
+# print("ST1001")
+# print("bob@gmail.com")
+
+#Q2
+# print("Bob\nST1001\nbob@gmail.com")
+
+#Q3
+# num1 = 14
+# num2 = 7
+# print(f"{num1} + {num2} = {num1 + num2}")
+# print(f"{num1} - {num2} = {num1 - num2}")
+# print(f"{num1} * {num2} = {num1 * num2}")
+# print(f"{num1} / {num2} = {num1 / num2}")
+
+#Q4
+
+# print('1\n2\n3\n4\n5')
+
+#Q5
+
+# print("\"SDK\" stands for \"Software Development Kit\", whereas\n\"IDE\" stands for \"Integrated Development Environment\".")
+
+#Q6
+
+# print("python is an \"awesome\" language.")
+# print("python\n\t2023")
+# print('I\'m from Entri.\b')
+# print("\65")
+# print("\x65")
+# print("Entri", "2023", sep="\n")
+# print("Entri", "2023", sep="\b")
+# print("Entri", "2023", sep="*", end="\b\b\b\b")
+
+#Q7
+#
+num = 23
+# textnum = "57"
+# decimal = 98.3
+# print(type(num))
+# print(type(textnum))
+# print(type(decimal))
+# total = num + int(textnum) + decimal
+# print(f"Sum: {total}")
+# print(f"Type of sum: {type(total)}")
+
+#Q8
+# days_in_year = 365
+# hours_in_day = 24
+# minutes_in_hour = 60
+# total_minutes = days_in_year * hours_in_day * minutes_in_hour
+#
+# print(f"This program calculates the total number of minutes in a year.")
+# print(f"Days in a year: {days_in_year}")
+# print(f"Hours in a day: {hours_in_day}")
+# print(f"Minutes in an hour: {minutes_in_hour}")
+# print(f"Total minutes in a year: {total_minutes}")
+
+#Q9
+# name = input("Please enter your name: ")
+# print(f"Hi {name}, welcome to Python programming :)")
+
+
+#Q10
+pounds = float(input("Please enter amount in pounds: "))
+conversion_rate = 1.25
+dollars = pounds * conversion_rate
+print(f"£{pounds} are ${dollars}")
